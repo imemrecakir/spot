567ccabf05aa511fd828653a0c76ca192adb3a37
Emre Çakır <imemrecakir@gmail.com>
2023-07-06 20:42:03 +0300

    initial commit

--- Spot/ViewController.swift
+++ Spot/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "SPOT"
+        label.font = Fonts.regular.of(.size28)
+        label.textColor = .white
+        return label
+    }()
+    
+    private let labelDefault: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "SPOT"
+        label.font = .systemFont(ofSize: 28)
+        label.textColor = .white
+        return label
+    }()
+    
+    private let stackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(label)
+        stackView.addArrangedSubview(labelDefault)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+
+        ])
     }
-
-
 }
 
